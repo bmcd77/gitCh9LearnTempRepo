0749962ce0adafa8e5356a4ef2feacedde2e226c
bmcd77 <bmcd77@hotmail.com>
2020-10-28 14:39:08 +0000

    Make simple change by adding a url

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string user = "bmcd77";
+            string repo = "gitCh9LearnTempRepo";
+            Uri projectUri = new Uri($"https://github.com/{user}/{repo}");
+
+            Console.WriteLine($"This is a test of Github SCM from a Rob Green channel 9 video for {projectUri}");
         }
     }
 }
